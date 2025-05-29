93c64b3d37581d59e0a2f5edd33ae754479217d5
Einsteinonthebeach2025 <wolflikeme1989@gmail.com>
2025-05-29 16:08:54 +0400

    after dark mode change

--- app/(routes)/dashboard/security/page.js
+++ app/(routes)/dashboard/security/page.js
@@ -5,6 +5,3 @@ const SecurityPage = () => {
 };
 
 export default SecurityPage;
-
-// client id - 333665232876-3hn3qdvj0ljccm4fc69pudraa76r8243.apps.googleusercontent.com
-// client secret - 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
