62052ffc2e44b805f9c8afbf72e69544276fa950
Einsteinonthebeach2025 <wolflikeme1989@gmail.com>
2025-05-29 16:13:23 +0400

    boris

--- app/pages/home/Test.jsx
+++ app/pages/home/Test.jsx
@@ -6,6 +6,10 @@ const Test = () => {
     <div className="h-screen  w-full center p-8 transition-colors duration-300">
       <div className="border p-3 bg-neutral-300 text-red-500 dark:bg-neutral-800 dark:text-blue-500">
         <h1>boris brejcha</h1>
+        <h1>boris brejcha</h1>
+        <h1>boris brejcha</h1>
+        <h1>boris brejcha</h1>
+        <h1>boris brejcha</h1>
       </div>
     </div>
   );
