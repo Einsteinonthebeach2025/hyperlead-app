39e43891ab543da6ae55a36b4ab0da23354d8d68
Einsteinonthebeach2025 <wolflikeme1989@gmail.com>
2025-07-22 19:30:44 +0200

    last test

--- app/lib/config/paypalConfig.js
+++ app/lib/config/paypalConfig.js
@@ -7,8 +7,10 @@ export const SUBSCRIPTION_PLANS = {
     annualPrice: "374",
     leads: 150,
     description: "Perfect for small businesses",
-    planId: "P-74J685623B288143WNBN7CQY",
-    annual_plan_id: "P-83F02828AK235573YNBUPPCY",
+    // planId: "P-74J685623B288143WNBN7CQY",
+    planId: "P-7XH25132HX613060MNBOTB5I",
+    // annual_plan_id: "P-83F02828AK235573YNBUPPCY",
+    annual_plan_id: "P-6UM118406D362493LNB37UZA",
   },
   PRO: {
     name: "PRO",
@@ -16,8 +18,10 @@ export const SUBSCRIPTION_PLANS = {
     annualPrice: "950",
     leads: 400,
     description: "Ideal for growing businesses",
-    planId: "P-2TJ41897NU456330ANBN7EEY",
-    annual_plan_id: "P-1T529730KX668460NNBUPPQY",
+    // planId: "P-2TJ41897NU456330ANBN7EEY",
+    planId: "P-9HX723217A653215GNBUSP5Q",
+    // annual_plan_id: "P-1T529730KX668460NNBUPPQY",
+    annual_plan_id: "P-8KK27710LB222034YNB37VHI",
   },
   HYPER: {
     name: "HYPER",
@@ -25,8 +29,10 @@ export const SUBSCRIPTION_PLANS = {
     annualPrice: "1720",
     leads: 800,
     description: "For large scale operations",
-    planId: "P-3M451725FG751784VNBN7FDY",
-    annual_plan_id: "P-3BM54016W0125070RNBUPQFI",
+    // planId: "P-3M451725FG751784VNBN7FDY",
+    planId: "P-5YB76014V2287162GNBUVGWQ",
+    // annual_plan_id: "P-3BM54016W0125070RNBUPQFI",
+    annual_plan_id: "P-4582297294762771VNB37VRI",
   },
 };
 
